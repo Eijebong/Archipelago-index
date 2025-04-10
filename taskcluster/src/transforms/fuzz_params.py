66cd7ca65f684f430ec78ffb69f43577739dd63d
Bastien Orivel <eijebong@bananium.fr>
2025-04-10 11:12:02 +0200

    Add support for skip output in fuzz transform

--- taskcluster/src/transforms/fuzz_params.py
+++ taskcluster/src/transforms/fuzz_params.py
@@ -9,6 +9,7 @@ parser = argparse.ArgumentParser(exit_on_error=False)
 parser.add_argument("-r", "--runs", default=100, type=int)
 parser.add_argument("-n", "--yamls_per_run", default="1", type=str)
 parser.add_argument("--classifier", default=None)
+parser.add_argument("--skip-output", default=False, action='store_true')
 
 @transforms.add
 def fuzz_params(config, tasks):
@@ -31,7 +32,9 @@ def fuzz_params(config, tasks):
 
     extra_args = ""
     if args.classifier:
-        extra_args = "--classifier " + shlex.quote(args.classifier)
+        extra_args += " --classifier " + shlex.quote(args.classifier)
+    if args.skip_output:
+        extra_args += " --skip-output"
 
     for task in tasks:
         env = task["worker"].setdefault("env", {})
